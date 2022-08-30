e3cfd10905057154e1af1057483563e40ad03df8
Puru573 <purukumar.p@gmail.com>
2022-08-30 14:10:12 +0530

    genre rendering

--- movies/src/Components/Favourites.js
+++ movies/src/Components/Favourites.js
@@ -5,13 +5,45 @@ export default class Favourites extends Component {
   constructor(){
     super();
     this.state={
-      movies:[]
+      movies:[],
+      genre:[],
+      currGenre:"All genre"
     }
   }
   async componentDidMount(){
     let ans=await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=a70924997e02110adcecdf0c4fa26bff&language=en-US&page=1`);
+    let genreId = {
+      28: "Action",
+      12: "Adventure",
+      16: "Animation",
+      35: "Comedy",
+      80: "Crime",
+      99: "Documentary",
+      18: "Drama",
+      10751: "Family",
+      14: "Fantasy",
+      36: "History",
+      27: "Horror",
+      10402: "Music",
+      9648: "Mystery",
+      10749: "Romance",
+      878: "Sci-Fi",
+      10770: "TV",
+      53: "Thriller",
+      10752: "War",
+      37: "Western",
+    };
+    let genreArr=[];
+    ans.data.results.map((movieObj)=>{
+      if(!genreArr.includes(genreId[movieObj.genre_ids[0]])){
+        genreArr.push(genreId[movieObj.genre_ids[0]]);
+      }
+    });
+    genreArr.unshift("All genre");
+    console.log(genreArr);
     this.setState({
       movies:[...ans.data.results],
+      genre:[...genreArr]
     });
   }
   render() {
@@ -40,10 +72,13 @@ export default class Favourites extends Component {
       <div class="row">
         <div class="col-3 favourite-list">
           <ul class="list-group">
-            <li class="list-group-item active" aria-current="true">ALL GENRE</li>
-            <li class="list-group-item">ROMANCE</li>
-            <li class="list-group-item">FANTASY</li>
-            <li class="list-group-item">HORROR</li>
+            {this.state.genre.map((genre)=>(
+              this.state.currGenre==genre ?
+            <li class="list-group-item active " aria-current="true">{genre}</li> :
+            <li class="list-group-item  " aria-current="true">{genre}</li> 
+            ))}
+ 
+        
 
           </ul>
         </div>
